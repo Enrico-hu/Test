9ff2a31efe20e9ab64560436ec37a633c9d1dbb2
Enrico Brachetti <e.brachetti@accenture.com>
2020-08-26 15:20:05 +0200

    Add projevct file to gitignore. Add TestDivide to AllTest

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestCount.class, TestSquare.class, SumTest.class})
+@SuiteClasses({ TestCount.class, TestSquare.class, SumTest.class, TestDivide.class})
 public class AllTests {
 
 }
